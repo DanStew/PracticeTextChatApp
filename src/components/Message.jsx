0374a7331b9694a354e33c7a9c57241bafa93683
DanStew <90531180+DanStew@users.noreply.github.com>
2023-08-01 18:44:26 +0100

    Implementing firebase hosting

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,6 +2,7 @@
 import { useContext, useEffect, useRef } from 'react'
 import { AuthContext } from '../context/AuthContext'
 import { ChatContext } from '../context/ChatContext'
+import { Timestamp } from 'firebase/firestore'
 
 function Message({message}){
 
@@ -15,12 +16,15 @@ function Message({message}){
         ref.current?.scrollIntoView({behavior:"smooth"})
     },[message])
 
+    function findTime(){
+        
+    }
 
     return(
         <div ref={ref} className={`message ${message.senderId === currentUser.uid && "owner"}`}>
             <div className="messageInfo">
                 <img src={message.senderId === currentUser.uid ? currentUser.photoURL : data.user.photoURL} alt="" />
-                <span>Just Now</span>
+                <span>{findTime()}</span>
             </div>
             <div className="messageContent">
                 <p>{message.text}</p>
